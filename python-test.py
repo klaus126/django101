61b58bab0ad7ae791467ea567fbe0dd095475e25
Arthur <arthur@Arthurui-MacBook-Pro.local>
2020-01-03 00:14:43 +0900

    first-commit

---
+++ python-test.py
@@ -0,0 +1,8 @@
+participant = {'name': 'Ola', 'country': 'Poland', 'favorite_numbers': [7, 42, 92]}
+#print(participant)
+
+#for key in participant:
+#	print(key, ":", participant[key])
+
+for key, value in participant.items():
+	print(key, ":", value)
